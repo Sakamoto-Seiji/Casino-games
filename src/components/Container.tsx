022f4618a49739943cf11050bca370393044ae0d
Sakamoto <sakamotoseiji9@gmail.com>
2024-12-15 21:44:05 +0900

    fix errors

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,17 +1,13 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode } from "react";
 
 // Define the props for the Container component
 interface ContainerProps {
-    children: ReactNode;
-    className?: string;
+  children: ReactNode;
+  className?: string;
 }
 
-const Container: React.FC<ContainerProps> = ({ children, className = '' }) => {
-    return (
-        <div className={`${className} flex-1`}>
-            {children}
-        </div>
-    );
+const Container: React.FC<ContainerProps> = ({ children, className = "" }) => {
+  return <div className={`${className} flex-1`}>{children}</div>;
 };
 
 export default Container;
